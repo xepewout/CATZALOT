5fe8cb9e8e461a5b17aeb577f8af8b6b8e5d0e1e
jorryns <111144938+xepewout@users.noreply.github.com>
2024-09-25 20:16:42 -0700

    0.0042
    -finished inventory system
    -added catzalots cape as a equipable
    -added hotbar
    TODO:
    -chest in room doesn't work
    -interact with potty

--- catzalot/main.gd
+++ catzalot/main.gd
@@ -6,6 +6,9 @@ const PickUp = preload("res://item/pick_up/pick_up.tscn")
 @onready var inventory_interface: Control = $HUD/InventoryInterface
 # Reference to the RoomContainer node where rooms will be dynamically loaded
 @onready var room_container: Node = $RoomContainer
+@onready var hot_bar_inventory = $HUD/HotBarInventory
+
+
 # Keeps track of the currently loaded room's path
 var current_room_path: String = ""
 
@@ -13,10 +16,12 @@ var current_room_path: String = ""
 func load_room(room_path: String) -> void:
 	# If the requested room is already loaded, do nothing
 	if current_room_path == room_path:
+		print("1")
 		return
 
 	# Unload the current room if any exists
 	if room_container.get_child_count() > 0:
+		print("2")
 		room_container.get_child(0).queue_free()
 
 	# Load the new room scene using @GDScript.Load for performance
@@ -26,6 +31,7 @@ func load_room(room_path: String) -> void:
 
 	# Update the current room path
 	current_room_path = room_path
+	print(current_room_path)
 
 func new_game():
 	load_room("res://rooms/test_room.tscn")
@@ -35,17 +41,23 @@ func _ready() -> void:
 	
 	player.toggle_inventory.connect(toggle_inventory_interface)
 	inventory_interface.set_player_inventory_data(player.inventory_data)
+	inventory_interface.set_equip_inventory_data(player.equip_inventory_data)
+	inventory_interface.force_close.connect(toggle_inventory_interface)
+	hot_bar_inventory.set_inventory_data(player.inventory_data)
+	
 	for node in get_tree().get_nodes_in_group("external_inventory"):
 		node.toggle_inventory.connect(toggle_inventory_interface)
+	
 
 func toggle_inventory_interface(external_inventory_owner = null) -> void:
 	inventory_interface.visible = not inventory_interface.visible
 	
 	if inventory_interface.visible:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		hot_bar_inventory.hide()
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
+		hot_bar_inventory.show()
 	if external_inventory_owner and inventory_interface.visible:
 		inventory_interface.set_external_inventory(external_inventory_owner)
 	else:
